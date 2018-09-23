61ce7583a9893dffcd2244f057c00714ce2c340c
naorellana <1005alexis@gmail.com>
2018-09-22 20:49:55 -0600

    creando proyecto con dependencias en maven

---
+++ src/main/java/com/norellanac/courier2018/Courier2018Application.java
@@ -0,0 +1,12 @@
+package com.norellanac.courier2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courier2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Courier2018Application.class, args);
+	}
+}
